c20c7fd7fb9bdcfae2aebd409795142f1f4e1a52
David <david.nh@comcast.net>
2017-01-21 14:54:41 -0500

    Intital intake subsystem

---
+++ src/org/usfirst/frc/team166/robot/subsystems/Intake.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team166.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Intake extends Subsystem {
+	// 2 states: on&off.
+	//
+	//
+	//
+	//
+
+	@Override
+	public void initDefaultCommand() {
+
+	}
+
+	public void on() {
+	}
+
+	public void off() {
+	}
+
+}
