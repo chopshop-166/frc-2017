faae9b31b029f5c273ff849b246a73a017c121f9
Quinlan Duval <quinlan.duval@comcast.net>
2017-01-21 15:06:48 -0500

    Initial shooter subsystem

---
+++ src/org/usfirst/frc/team166/robot/subsystems/Shooter.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team166.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Shooter extends Subsystem {
+
+	public void initDefaultCommand() {
+	}
+
+	public void start() {
+
+	}
+
+	public void stop() {
+
+	}
+
+	public void shoot() {
+
+	}
+}
