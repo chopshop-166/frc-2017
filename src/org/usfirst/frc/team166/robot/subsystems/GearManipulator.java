6018d74a6cae4e36685c3fac001bffe2b214fdd8
Alex Jobin <alexpjobin@gmail.com>
2017-01-21 14:40:56 -0500

    Initial methods for GearManipulator subsytem

---
+++ src/org/usfirst/frc/team166/robot/subsystems/GearManipulator.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team166.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class GearManipulator extends Subsystem {
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+
+	}
+
+	public void open() {
+	}
+
+	public void close() {
+	}
+}
