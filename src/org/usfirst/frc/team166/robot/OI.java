43732b3ba4f254c377a892309fbc69f6289c1bf0
Mark Oden <mwoden@gmail.com>
2017-01-22 14:31:18 -0500

    Add the subsystem instances to the Robot class

--- src/org/usfirst/frc/team166/robot/OI.java
+++ src/org/usfirst/frc/team166/robot/OI.java
@@ -1,12 +1,8 @@
 package org.usfirst.frc.team166.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
-
-import org.usfirst.frc.team166.robot.commands.ExampleCommand;
-
 /**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
+ * This class is the glue that binds the controls on the physical operator interface to the commands and command groups
+ * that allow control of the robot.
  */
 public class OI {
 	//// CREATING BUTTONS
--- src/org/usfirst/frc/team166/robot/Robot.java
+++ src/org/usfirst/frc/team166/robot/Robot.java
@@ -8,40 +8,43 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team166.robot.commands.ExampleCommand;
-import org.usfirst.frc.team166.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team166.robot.subsystems.Drive;
+import org.usfirst.frc.team166.robot.subsystems.GearManipulator;
+import org.usfirst.frc.team166.robot.subsystems.Intake;
+import org.usfirst.frc.team166.robot.subsystems.Shooter;
+import org.usfirst.frc.team166.robot.subsystems.Storage;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
+ * described in the IterativeRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the manifest file in the resource directory.
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final Drive drive = new Drive();
+	public static final GearManipulator gearManipulator = new GearManipulator();
+	public static final Intake intake = new Intake();
+	public static final Shooter shooter = new Shooter();
+	public static final Storage storage = new Storage();
+
 	public static OI oi;
 
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
 
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
 		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 	}
 
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You can use it to reset any subsystem
+	 * information you want to clear when the robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
@@ -54,25 +57,22 @@ public class Robot extends IterativeRobot {
 	}
 
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes using
+	 * the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the auto name from the text box below the
+	 * Gyro
 	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented
+	 * example) or additional comparisons to the switch structure below with additional strings & commands.
 	 */
 	@Override
 	public void autonomousInit() {
 		autonomousCommand = chooser.getSelected();
 
 		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
+		 * String autoSelected = SmartDashboard.getString("Auto Selector", "Default"); switch(autoSelected) { case
+		 * "My Auto": autonomousCommand = new MyAutoCommand(); break; case "Default Auto": default: autonomousCommand =
+		 * new ExampleCommand(); break; }
 		 */
 
 		// schedule the autonomous command (example)
