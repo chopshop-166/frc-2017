902057d04e444f42d067e72a29259f525a9101de
IanCummings166 <3salamanders@myfairpoint.net>
2017-02-17 08:12:28 -0500

    Vision and storage
    Began construction of basic vision

--- src/org/usfirst/frc/team166/robot/subsystems/Storage.java
+++ src/org/usfirst/frc/team166/robot/subsystems/Storage.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team166.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -15,7 +16,8 @@ public class Storage extends Subsystem {
 	}
 
 	public void agitatorOn() {
-		exampleVictor.set(1.0);
+		exampleVictor.set(Preferences.getInstance().getDouble("Forward", 1.0));
+
 	}
 
 	public void agitatorOff() {
