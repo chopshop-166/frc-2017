c631f951ff66545e2a62e09fb7fe4f45c0ccc54c
Matt Soucy <msoucy@csh.rit.edu>
2017-02-19 17:00:32 -0500

    Removed duplicated variable introduced in merge

--- src/org/usfirst/frc/team166/robot/RobotMap.java
+++ src/org/usfirst/frc/team166/robot/RobotMap.java
@@ -60,5 +60,4 @@ public class RobotMap {
 	public static String autoWaitTime = "Center Gear Auto Wait Time";
 	public static String autoWaitTime2 = "Center Gear Auto Wait Time 2 ";
 	public static String centerGearAutoDistanceBackwards = "Center Gear Auto Distance Backwards";
-	public static String elevatorMotorSpeed = "Elevator Motor Speed";
 }
